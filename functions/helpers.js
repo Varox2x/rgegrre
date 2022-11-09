97959a2f355ff2523c1fd4d5318716e2f61dc102
Hubert Rutkowski <hubr2000@gmail.com>
2022-11-09 14:45:59 +0100

    first commit

---
+++ functions/helpers.js
@@ -0,0 +1,10 @@
+function generaterandomEmail() {
+    var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    var string = '';
+    for(var ii=0; ii<15; ii++){
+        string += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return string+="@gmail.com"  
+}
+
+module.exports = { generaterandomEmail }
